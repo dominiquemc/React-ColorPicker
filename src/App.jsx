4d2af7cdb6b60ff8f9d4f6e0e91a997125ef17ee
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-06-13 21:45:16 -0500

    added Color component, and useState to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
+
 // Write your Color component here
 
+const Color = ({ color, setSelectedColor }) => {
+  return <div className={color} onClick={() => setSelectedColor(color)}></div>;
+};
+
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color="violet" setSelectedColor={setSelectedColor} />
+        <Color color="blue" setSelectedColor={setSelectedColor} />
+        <Color color="orange" setSelectedColor={setSelectedColor} />
+      </div>
     </div>
   );
 };
 
 export default App;
+
+// notes:
+// useState takes an initial state and returns a function that returns the value of the state on each commit.
+// selectedColor: is the picker component which will pass values down to its child Color components
+// setSelectedColor: is the function.
